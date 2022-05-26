2c209b008e1d83fc8d4a3ff5b31711cdcb9e9bd3
Akila-Piumal <akilapiumal16@gmail.com>
2022-05-26 15:57:36 +0530

    Added System

---
+++ src/lk/ijse/pos/controller/LoginFormController.java
@@ -0,0 +1,41 @@
+package lk.ijse.pos.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class LoginFormController {
+    public JFXTextField txtUserName;
+    public JFXPasswordField pwdPassword;
+    public CheckBox cbShowPassword;
+    public JFXTextField txtPassword;
+
+    public void initialize(){
+        txtPassword.textProperty().bind(pwdPassword.textProperty());
+        txtPassword.visibleProperty().bind(cbShowPassword.selectedProperty());
+        pwdPassword.visibleProperty().bind(cbShowPassword.selectedProperty().not());
+    }
+
+    
+    public void keyReleased(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            if (txtUserName.getText() != null) {
+                pwdPassword.requestFocus();
+                if (pwdPassword.getText().length()>0){
+                    loginToSystem();
+                }
+            }
+        }
+    }
+
+    private void loginToSystem() {
+
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
