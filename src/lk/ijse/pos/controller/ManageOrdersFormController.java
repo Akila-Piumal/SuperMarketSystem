59173dd2c1bba74e3a2f6b684e76458fde9ef52b
Akila-Piumal <akilapiumal16@gmail.com>
2022-05-30 00:26:10 +0530

    Search a order of a customer with order id success

--- src/lk/ijse/pos/controller/ManageOrdersFormController.java
+++ src/lk/ijse/pos/controller/ManageOrdersFormController.java
@@ -3,13 +3,14 @@ package lk.ijse.pos.controller;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import lk.ijse.pos.bo.BOFactory;
 import lk.ijse.pos.bo.Custom.ManageOrderBO;
-import lk.ijse.pos.bo.SuperBO;
 import lk.ijse.pos.dto.CustomerDTO;
 import lk.ijse.pos.dto.OrderDTO;
 
@@ -30,30 +31,32 @@ public class ManageOrdersFormController {
     public Label lblPrice;
     public Label lblDiscount;
     public Label lblTotalPrice;
+    public JFXTextField txtSearchOrders;
 
     ManageOrderBO manageOrderBO = (ManageOrderBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.MANAGEORDER);
 
-    public void initialize(){
+    public void initialize() {
         loadAllCustomerIDS();
 
         cmbSelectCustomer.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, selectedCustomerID) -> {
             cmbOrderID.getItems().clear();
-            loadOrderIDSForEachCustomer(selectedCustomerID);
+            try {
+                ArrayList<OrderDTO> eachCustomerOrders = getOrdersForEachCustomer(selectedCustomerID);
+                for (OrderDTO eachCustomerOrder : eachCustomerOrders) {
+                    cmbOrderID.getItems().add(eachCustomerOrder.getOrderId());
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         });
 
     }
 
-    private void loadOrderIDSForEachCustomer(String custID) {
-        try {
-            ArrayList<OrderDTO> eachCustomerOrders = manageOrderBO.getEachCustomerOrders(custID);
-            for (OrderDTO eachCustomerOrder : eachCustomerOrders) {
-                cmbOrderID.getItems().add(eachCustomerOrder.getOrderId());
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    private ArrayList<OrderDTO> getOrdersForEachCustomer(String custID) throws SQLException, ClassNotFoundException {
+        ArrayList<OrderDTO> eachCustomerOrders = manageOrderBO.getEachCustomerOrders(custID);
+        return eachCustomerOrders;
     }
 
     private void loadAllCustomerIDS() {
@@ -69,13 +72,48 @@ public class ManageOrdersFormController {
         }
     }
 
+
     public void backToHomeOnAction(MouseEvent mouseEvent) {
     }
 
-    public void textFields_Key_Released(KeyEvent keyEvent) {
+    public void textFields_Key_Pressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            try {
+                if (checkTheOrderIsCorrect()) {
+                    cmbOrderID.getSelectionModel().select(txtSearchOrders.getText());
+                } else {
+                    new Alert(Alert.AlertType.ERROR,"Order Id not associated with this customer").show();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean checkTheOrderIsCorrect() throws SQLException, ClassNotFoundException {
+        ArrayList<OrderDTO> ordersForEachCustomer = getOrdersForEachCustomer(cmbSelectCustomer.getValue());
+        for (OrderDTO orderDTO : ordersForEachCustomer) {
+            if (txtSearchOrders.getText().equalsIgnoreCase(orderDTO.getOrderId())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void btnSearchOrderOnAction(ActionEvent actionEvent) {
+        try {
+            if (checkTheOrderIsCorrect()) {
+                cmbOrderID.getSelectionModel().select(txtSearchOrders.getText());
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Order Id not associated with this customer").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void btnUpdateItemDetailsOnAction(ActionEvent actionEvent) {
@@ -86,4 +124,6 @@ public class ManageOrdersFormController {
 
     public void btnCancelOrderOnAction(ActionEvent actionEvent) {
     }
+
+
 }
